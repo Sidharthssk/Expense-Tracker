081d36d8922400c301cb97ddb2a6137bd67f256d
Sidharthssk <sidharthssk.18@gmail.com>
2022-10-26 12:27:51 +0530

    changed display property of home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ function Home() {
       <div className="container-md text-center" style={{height: "90%"}}>
         <h1 className="mt-3" id="mainHeading" style={{color: "white"}}>Welcome to Expense Tracker...</h1>
         <h3 className="my-3" style={{color: "white"}}>The place where you can keep a track of your expense efficiently</h3>
-        <div className="row d-flex align-items-md-center justify-content-center " >
+        <div className="row d-sm-inline-flex align-items-md-center justify-content-center " >
           <div className="col mx-lg-5 my-3">
             <Card />
           </div>
