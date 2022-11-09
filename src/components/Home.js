212dc83bb09c8f29fc329722758c7c5af98accc5
Sidharthssk <sidharthssk.18@gmail.com>
2022-11-09 20:27:18 +0530

    Fixed responsive issue of the home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,11 +9,11 @@ function Home() {
       <div className="container-md text-center" style={{height: "90%"}}>
         <h1 className="mt-3" id="mainHeading" style={{color: "white"}}>Welcome to Expense Tracker...</h1>
         <h3 className="my-3" style={{color: "white"}}>The place where you can keep a track of your expense efficiently</h3>
-        <div className="row d-sm-inline-flex align-items-md-center justify-content-center " >
-          <div className="col mx-lg-5 my-3">
+        <div className="row align-items-md-center justify-content-center " >
+          <div className="col-md-6 mx-lg-5 my-3">
             <Card />
           </div>
-          <div className="col">
+          <div className="col-md-6">
             <ExpenseContainer />
           </div>
         </div>
